fc0ae2611896f09088aae9cf8a93fa4cfdf44369
Tim Kent <timkentdev@gmail.com>
2024-09-29 16:28:25 -0700

    restore app controller, service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { BirdsController } from './birds/birds.controller';
 
 @Module({
   imports: [],
-  controllers: [BirdsController],
-  providers: [],
+  controllers: [AppController, BirdsController],
+  providers: [AppService],
 })
 export class AppModule {}
