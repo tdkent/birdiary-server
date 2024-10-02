f389d1d8726f70099de4ef3ffe4b1fcfb7379fb3
Tim Kent <timkentdev@gmail.com>
2024-10-01 18:15:19 -0700

    add custom create user error msgs. add global validation whitelist rule.

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -1,12 +1,18 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
-  @IsEmail()
+  @IsEmail({}, { message: 'Please submit a valid email address.' })
   email: string;
 
   @IsString()
+  @Length(8, 36, {
+    message: 'Password must be between 8 and 36 characters.',
+  })
   password: string;
 
   @IsString()
+  @Length(0, 36, {
+    message: 'Name cannot be longer than 36 characters.',
+  })
   name: string;
 }
