1ecd32ef8b952ddd874b405415c6a13907e903f8
Tim Kent <timdekent@gmail.com>
2024-10-03 12:44:55 -0700

    make dto fields readonly

--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -2,17 +2,17 @@ import { IsEmail, IsString, Length } from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail({}, { message: 'Please submit a valid email address.' })
-  email: string;
+  readonly email: string;
 
   @IsString()
   @Length(8, 36, {
     message: 'Password must be between 8 and 36 characters.',
   })
-  password: string;
+  readonly password: string;
 
   @IsString()
   @Length(0, 36, {
     message: 'Name cannot be longer than 36 characters.',
   })
-  name: string;
+  readonly name: string;
 }
