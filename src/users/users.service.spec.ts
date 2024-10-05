1658b2c8e775fed89a67b2faf1df7d93fd5ea6a4
Tim Kent <timdekent@gmail.com>
2024-10-05 14:47:37 -0700

    add user service method unit tests: findById, findByEmail

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { NotFoundException } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { DatabaseService } from '../database/database.service';
 
 describe('UsersService', () => {
   let service: UsersService;
+  const testId = 1;
+  const testEmail = 'tim@tim.me';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -15,5 +18,32 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll method', () => {
+    it('returns an array of users', async () => {
+      const users = await service.findAll();
+      expect(users.length).toBeGreaterThanOrEqual(1);
+    });
+  });
+
+  describe('findById method', () => {
+    it('returns the requested user', async () => {
+      const user = await service.findById(testId);
+      expect(user).not.toBeNull();
+      expect(user.email).toBe(testEmail);
+    });
+
+    it('throws 404 if user not found', async () => {
+      await expect(service.findById(-1)).rejects.toThrow(NotFoundException);
+    });
+  });
+
+  describe('findByEmail method', () => {
+    it('returns the requested user', async () => {
+      const user = await service.findByEmail(testEmail);
+      expect(user).not.toBeNull();
+      expect(user.id).toBe(testId);
+    });
   });
 });
