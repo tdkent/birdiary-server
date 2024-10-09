b2ff6242672d8073199b9f13b54b63e0ccd1f6ee
Tim Kent <timkentdev@gmail.com>
2024-10-09 16:34:40 -0700

    make update profile dto fields optional

--- src/users/dtos/update-profile.dto.ts
+++ src/users/dtos/update-profile.dto.ts
@@ -1,14 +1,16 @@
-import { IsString, Length } from 'class-validator';
+import { IsOptional, IsString, MaxLength } from 'class-validator';
 
 export class UpdateProfileDto {
+  @IsOptional()
   @IsString()
-  @Length(0, 36, {
+  @MaxLength(36, {
     message: 'Name cannot be longer than 36 characters',
   })
   readonly name: string;
 
+  @IsOptional()
   @IsString()
-  @Length(0, 60, {
+  @MaxLength(60, {
     message: 'Location cannot be longer than 60 characters',
   })
   readonly location: string;
