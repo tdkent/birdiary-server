30d698b403b91ae0dcbed5177b0a2bb24102188c
Tim Kent <timkentdev@gmail.com>
2024-10-13 17:43:33 -0700

    add life list query

--- src/sightings/lifelist.service.ts
+++ src/sightings/lifelist.service.ts
@@ -1,10 +1,15 @@
-import { DatabaseService } from 'src/database/database.service';
+import { Injectable } from '@nestjs/common';
+import { DatabaseService } from '../database/database.service';
 
+@Injectable()
 export class LifeListService {
   constructor(private readonly databaseService: DatabaseService) {}
 
   //---- FETCH USER'S LIFE LIST
   findLifeList(id: number) {
-    return `id ${id} life list`;
+    return this.databaseService.sighting.findMany({
+      where: { user_id: id },
+      distinct: ['bird_id'],
+    });
   }
 }
