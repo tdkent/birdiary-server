52a05088be6af2af4a086c9dcbace22dd2dbd098
Tim Kent <timkentdev@gmail.com>
2025-02-08 18:00:20 -0800

    chore: add permissive CORS policy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { port } from './common/constants/env.constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
