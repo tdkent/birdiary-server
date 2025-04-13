aa26d163db7397a8ed601282da519357a327b271
Tim Kent <timkentdev@gmail.com>
2025-04-12 21:17:09 -0700

    feat: add page query to DTO for pagination

--- src/bird/dto/get-birds-by-alpha.dto.ts
+++ src/bird/dto/get-birds-by-alpha.dto.ts
@@ -1,12 +1,19 @@
-import { IsString, Length, Matches } from 'class-validator';
+import { IsInt, IsOptional, Length, Matches, Max, Min } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class GetBirdsByAlphaDto {
-  @IsString()
+  @IsOptional()
   @Length(1, 1, {
-    message: 'Query string must be a single character A-Z',
+    message: '"startsWith" query must be a single character A-Z',
   })
   @Matches(/[A-Z]/, {
-    message: 'Query string must be a single character A-Z',
+    message: '"startsWith" query must be a single character A-Z',
   })
   readonly startsWith: string;
+
+  @Type(() => Number)
+  @IsInt({ message: '"page" must be an integer 1-34' })
+  @Min(1, { message: '"page" must be an integer 1-34' })
+  @Max(34, { message: '"page" must be an integer 1-34' })
+  readonly page: number;
 }
