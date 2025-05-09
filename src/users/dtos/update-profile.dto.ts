3e19a6524066b50f0072b443be5f73202871774d
Tim Kent <timkentdev@gmail.com>
2025-05-08 22:01:32 -0700

    refactor: update edit profile dto and service

--- src/users/dtos/update-profile.dto.ts
+++ src/users/dtos/update-profile.dto.ts
@@ -1,14 +1,12 @@
-import { IsOptional, IsString, MaxLength } from 'class-validator';
+import { IsString, MaxLength } from 'class-validator';
 
 export class UpdateProfileDto {
-  @IsOptional()
   @IsString()
-  @MaxLength(60, {
+  @MaxLength(24, {
     message: 'Name cannot be longer than 60 characters',
   })
   readonly name: string;
 
-  @IsOptional()
   @IsString()
   @MaxLength(120, {
     message: 'Location cannot be longer than 120 characters',
