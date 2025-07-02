694eecbea12fad6460b85de6c7d9ca9893052c0c
Tim Kent <timkentdev@gmail.com>
2025-07-02 15:51:44 -0700

    refactor: update user seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,31 +1,25 @@
 import { PrismaClient } from '@prisma/client';
 import { hashPassword } from '../src/common/helpers/auth.helpers';
-import { testUserPw } from '../src/common/constants/env.constants';
+import {
+  testUserEmail,
+  testUserPassword,
+} from '../src/common/constants/env.constants';
 import { birds } from '../db/birds.json';
-import type { Bird } from 'src/common/models/bird.model';
+import type { Bird } from 'src/common/models/db.model';
 
 const prisma = new PrismaClient();
 
 async function main() {
   await prisma.bird.createMany({
-    data: birds as unknown as Bird,
+    data: birds as unknown as Omit<Bird, 'id'>,
   });
 
   await prisma.user.create({
     data: {
-      email: 'tim@tim.me',
-      password: await hashPassword(testUserPw),
-      profile: {
-        create: {
-          name: '',
-          location: '',
-        },
-      },
-      favBird: {
-        create: {
-          birdId: null,
-        },
-      },
+      email: testUserEmail,
+      password: await hashPassword(testUserPassword),
+      name: 'Tim',
+      favoriteBirdId: 116,
     },
   });
 }
