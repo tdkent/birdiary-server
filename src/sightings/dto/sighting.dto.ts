0daad15ed4dc89fb7ad7188b7e4cf08ea7d62a1f
Tim Kent <timkentdev@gmail.com>
2025-07-06 11:55:39 -0700

    refactor: add consolidated sighting dto

---
+++ src/sightings/dto/sighting.dto.ts
@@ -0,0 +1,77 @@
+import { PickType } from '@nestjs/swagger';
+import {
+  IsDate,
+  IsIn,
+  IsInt,
+  IsObject,
+  IsOptional,
+  IsString,
+  MaxDate,
+  MaxLength,
+  Min,
+  Max,
+  MinDate,
+  ValidateIf,
+  ValidateNested,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+import { LocationDto } from '../../locations/dto/location.dto';
+import { BIRD_COUNT } from 'src/common/constants/api.constants';
+
+class SightingDto {
+  @Type(() => Number)
+  @IsInt()
+  readonly id: number;
+
+  @Type(() => Number)
+  @IsInt()
+  readonly userId: number;
+
+  @IsInt()
+  @Min(1)
+  @Max(BIRD_COUNT)
+  readonly birdId: number;
+
+  @ValidateIf((_, value) => value !== null)
+  @IsInt()
+  readonly locationId: number;
+
+  @IsDate()
+  @Type(() => Date)
+  @MinDate(new Date('1950-01-01'))
+  @MaxDate(() => new Date())
+  readonly date: Date;
+
+  @IsString()
+  @MaxLength(150)
+  @ValidateIf((_, value) => value !== null)
+  readonly description: string;
+}
+
+export class CreateSightingDto extends PickType(SightingDto, [
+  'birdId',
+  'date',
+  'description',
+] as const) {
+  @IsOptional()
+  @IsObject()
+  @ValidateNested()
+  @Type(() => LocationDto)
+  location: LocationDto;
+}
+
+export class GetSightingsDto {
+  @IsOptional()
+  @IsIn(['date', 'bird', 'location', 'lifelist'], { message: 'Invalid query' })
+  readonly groupBy?: string;
+
+  @IsOptional()
+  @IsIn(['alphaAsc', 'alphaDesc', 'count', 'dateAsc', 'dateDesc'])
+  readonly sortBy: string;
+
+  // @IsOptional()
+  @Type(() => Number)
+  @IsInt({ message: 'Invalid query' })
+  @Min(1, { message: 'Invalid query' })
+  readonly page: number;
+}
