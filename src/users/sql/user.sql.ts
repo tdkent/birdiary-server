36623420bb6f81bbd8614e76ced218a37b500b4c
Tim Kent <timkentdev@gmail.com>
2025-10-06 21:55:33 -0700

    feat: add data for top 3 sightings, dates, and locations

--- src/users/sql/user.sql.ts
+++ src/users/sql/user.sql.ts
@@ -7,12 +7,18 @@ export function getUserSightingStats(
   return Prisma.sql`
     -- CTE 1: All sightings
     WITH all_sightings AS (
-      SELECT * FROM "Sighting"
-      WHERE "userId" = ${userId}
+      SELECT *
+      FROM "Sighting"
+      JOIN "Bird"
+      ON "birdId" = "Bird".id
+      LEFT JOIN "Location" -- Include rows w/null values
+      ON "locationId" = "Location".id
+      WHERE "Sighting"."userId" = ${userId}
     ),
     -- CTE 2: All fav bird sightings
     fav_sightings AS (
-      SELECT * FROM "Sighting"
+      SELECT *
+      FROM "Sighting"
       WHERE "userId" = ${userId}
       AND "birdId" = ${favoriteBirdId}
     )
@@ -28,25 +34,19 @@ export function getUserSightingStats(
       (
         SELECT CAST(COUNT(*) AS int) 
         FROM all_sightings
-        JOIN "Bird" b
-        ON all_sightings."birdId" = b.id
-        WHERE b.rarity = 'Common'
+        WHERE rarity = 'Common'
       ) AS "countOfCommonSightings",
       -- Count of uncommon rarity sightings
       (
         SELECT CAST(COUNT(*) AS int) 
         FROM all_sightings
-        JOIN "Bird" b
-        ON all_sightings."birdId" = b.id
-        WHERE b.rarity = 'Uncommon'
+        WHERE rarity = 'Uncommon'
       ) AS "countOfUncommonSightings",
       -- Count of rare rarity sightings
       (
         SELECT CAST(COUNT(*) AS int) 
         FROM all_sightings
-        JOIN "Bird" b
-        ON all_sightings."birdId" = b.id
-        WHERE b.rarity = 'Rare'
+        WHERE rarity = 'Rare'
       ) AS "countOfRareSightings",
       -- Count fav sightings
       (SELECT CAST(COUNT(*) AS int) FROM fav_sightings) AS "countOfFavBirdSightings",
@@ -62,5 +62,44 @@ export function getUserSightingStats(
         FROM fav_sightings
         WHERE "birdId" = ${favoriteBirdId}
       ) AS "newestFavSighting",
+      -- Top 3 most-sighted birds
+      (
+        SELECT json_agg(
+          json_build_object('birdId', "birdId", 'commonName', "commonName", 'count', count)
+          )
+        FROM (
+          SELECT "birdId", "commonName", COUNT(*) AS count
+          FROM all_sightings
+          GROUP BY "birdId", "commonName"
+          ORDER BY count DESC, "commonName"
+          LIMIT 3
+        )
+      ) AS "topThreeBirds",
+      -- Top 3 locations by most sightings
+      (
+        SELECT json_agg(
+          json_build_object('locationId', "locationId", 'name', name, 'count', count)
+          )
+        FROM (
+          SELECT "locationId", "name", COUNT(*) AS count
+          FROM all_sightings
+          GROUP BY "locationId", "name"
+          ORDER BY count DESC, "name"
+          LIMIT 3
+        )
+      ) AS "topThreeLocations",
+      -- Top 3 dates by most sightings
+            (
+        SELECT json_agg(
+          json_build_object('date', "date", 'count', count)
+          )
+        FROM (
+          SELECT date, COUNT(*) AS count
+          FROM all_sightings
+          GROUP BY date
+          ORDER BY count DESC
+          LIMIT 3
+        )
+      ) AS "topThreeDates";
   `;
 }
