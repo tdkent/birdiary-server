1c968f09f85914f50b217e0ea25d15155c465868
Tim Kent <timkentdev@gmail.com>
2025-11-03 11:52:19 -0800

    feat: add query to count results. update search query with dynamic order by.

--- src/bird/sql/bird.sql.ts
+++ src/bird/sql/bird.sql.ts
@@ -1,6 +1,15 @@
 import { Prisma } from '@prisma/client';
 import { RESULTS_PER_PAGE } from '../../common/constants';
 
+export function getSearchCount(searchTerm: string): Prisma.Sql {
+  return Prisma.sql`
+    SELECT CAST(COUNT(id) AS int) AS count
+    FROM "Bird"
+    WHERE "commonName" ILIKE CONCAT('%', ${searchTerm}, '%')
+    OR family ILIKE CONCAT('%', ${searchTerm}, '%')
+  `;
+}
+
 export function getBirdsBySearchTerm(
   searchTerm: string,
   page: number,
@@ -8,6 +17,15 @@ export function getBirdsBySearchTerm(
   return Prisma.sql`
     SELECT *
     FROM "Bird"
+    WHERE "commonName" ILIKE CONCAT('%', ${searchTerm}, '%')
+    OR family ILIKE CONCAT('%', ${searchTerm}, '%')
+    ORDER BY
+      CASE
+        WHEN POSITION(LOWER(${searchTerm}) IN LOWER("commonName")) > 0 THEN 2
+        WHEN POSITION(LOWER(${searchTerm}) IN LOWER(family)) > 0 THEN 1
+        ELSE 0
+      END DESC,
+      id ASC
     LIMIT ${RESULTS_PER_PAGE}
     OFFSET ${RESULTS_PER_PAGE * (page - 1)}
   `;
